82d5e7efab336e6109d6d9dfa435994640b47fd5
Himanshu Gupta <bansalhimanshu47@yahoo.com>
2021-02-15 23:14:58 +0530

    Added Recursion

---
+++ Basics of Programming/Introduction to Recursion/PrintIncreasing.java
@@ -0,0 +1,19 @@
+// package Basics of Programming.Introduction to Recursion;
+import java.util.*;
+public class PrintIncreasing {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n) {
+        if(n==0)
+            return;
+        
+        printIncreasing(n-1);
+        System.out.println(n);
+            
+    }
+}
