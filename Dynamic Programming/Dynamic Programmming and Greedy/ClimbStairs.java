0e1056d39a8dbcd0779d73980cec7d64536f28ce
Himanshu Gupta <bansalhimanshu47@yahoo.com>
2021-02-18 18:31:38 +0530

    Added more Solutions

---
+++ Dynamic Programming/Dynamic Programmming and Greedy/ClimbStairs.java
@@ -0,0 +1,32 @@
+// package Dynamic Programming.Dynamic Programmming and Greedy;
+import java.util.*;
+public class ClimbStairs {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n+1];
+        int ans = ways(n, arr);
+        System.out.println(ans);
+    }
+    
+    public static int ways(int n, int[] dp){
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 0;
+        
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        
+        int n1 = ways(n-1, dp);
+        int n2 = ways(n-2, dp);
+        int n3 = ways(n-3, dp);
+        int ans = n1 + n2 + n3;
+        dp[n] = ans;
+        
+        return (n1 + n2 + n3);
+        
+    }
+}
---
+++ Dynamic Programming/Dynamic Programmming and Greedy/ClimbStarirsWithVariableJumps.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ClimbStarirsWithVariableJumps{
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int[] dp = new int[n+1];
+        int ans = climbStairsWithVariableJumps(arr, dp);
+        System.out.println(ans);
+        
+    }
+    
+    public static int climbStairsWithVariableJumps(int[] arr, int[] dp){
+        int n = arr.length;
+        dp[n] = 1;
+        
+        for(int i=n-1; i>=0; i--){
+            int jumps = arr[i];
+            for(int j=1; j<=jumps; j++){
+                if(i+j <= n)
+                    dp[i] = dp[i] + dp[i+j];
+            }
+        }
+        return dp[0];
+    }
+}
