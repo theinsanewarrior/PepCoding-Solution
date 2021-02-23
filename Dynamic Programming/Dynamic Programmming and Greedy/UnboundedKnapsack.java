c9453d9eda4862bc715fd6de36a928e46f0062cb
Himanshu Gupta <bansalhimanshu47@yahoo.com>
2021-02-23 10:49:26 +0530

    Added Knapsack Solution

---
+++ Dynamic Programming/Dynamic Programmming and Greedy/UnboundedKnapsack.java
@@ -0,0 +1,40 @@
+// package Dynamic Programming.Dynamic Programmming and Greedy;
+import java.util.*;
+public class UnboundedKnapsack {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] val = new int[n];
+        for(int i=0; i<n; i++){
+            val[i] = scn.nextInt();
+        }
+        int[] wt = new int[n];
+        for(int i=0; i<n; i++){
+            wt[i] = scn.nextInt();
+        }
+        int cap = scn.nextInt();
+        int[][] dp = new int[n+1][cap+1];
+        int ans = UnboundedKnapsack(val, wt, dp);
+        System.out.println(ans);
+    }
+    
+    public static int UnboundedKnapsack(int[] val, int[] wt, int[][] dp){
+        
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=1; j<dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                if(j-wt[i-1] >= 0 )
+                    dp[i][j] = Math.max((val[i-1]+dp[i][j-wt[i-1]]), dp[i-1][j] );
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
