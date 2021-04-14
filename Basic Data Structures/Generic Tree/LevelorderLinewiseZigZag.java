7970c50e042d2d07a9cc50b010a79821db598c39
Himanshu Gupta <bansalhimanshu47@yahoo.com>
2021-04-14 13:02:13 +0530

    added more generic tree solutions

---
+++ Basic Data Structures/Generic Tree/LevelorderLinewiseZigZag.java
@@ -0,0 +1,136 @@
+// package Basic Data Structures.Generic Tree;
+import java.util.*;
+public class LevelorderLinewiseZigZag {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+      }
+    
+      public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+          str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+    
+        for (Node child : node.children) {
+          display(child);
+        }
+      }
+    
+      public static Node construct(int[] arr) {
+        Node root = null;
+    
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+          if (arr[i] == -1) {
+            st.pop();
+          } else {
+            Node t = new Node();
+            t.data = arr[i];
+    
+            if (st.size() > 0) {
+              st.peek().children.add(t);
+            } else {
+              root = t;
+            }
+    
+            st.push(t);
+          }
+        }
+    
+        return root;
+      }
+    
+      public static int size(Node node) {
+        int s = 0;
+    
+        for (Node child : node.children) {
+          s += size(child);
+        }
+        s += 1;
+    
+        return s;
+      }
+    
+      public static int max(Node node) {
+        int m = Integer.MIN_VALUE;
+    
+        for (Node child : node.children) {
+          int cm = max(child);
+          m = Math.max(m, cm);
+        }
+        m = Math.max(m, node.data);
+    
+        return m;
+      }
+    
+        public static int height(Node node) {
+            int h = -1;
+            
+            for (Node child : node.children) {
+                int ch = height(child);
+                h = Math.max(h, ch);
+            }
+            h += 1;
+            
+            return h;
+        }
+    
+        public static void traversals(Node node){
+            System.out.println("Node Pre " + node.data);
+            
+            for(Node child: node.children){
+                System.out.println("Edge Pre " + node.data + "--" + child.data);
+                traversals(child);
+                System.out.println("Edge Post " + node.data + "--" + child.data);
+            }
+            
+            System.out.println("Node Post " + node.data);
+        }
+    
+        public static void levelOrderLinewiseZZ(Node node){
+            // write your code here
+            Stack<Node> main = new Stack<>();
+            Stack<Node> child = new Stack<>();
+            main.push(node);
+            int level = 0;
+            
+            while(!main.isEmpty()){
+                Node curr = main.pop();
+                if(level%2==0){
+                    for(int i=0; i<curr.children.size(); i++){
+                        child.push(curr.children.get(i));
+                    }
+                }
+                else{
+                    for(int i=curr.children.size()-1; i>=0; i-- ){
+                        child.push(curr.children.get(i) );
+                    }
+                }
+                
+                System.out.print(curr.data+ " ");
+                if(main.size() == 0){
+                    main = child;
+                    child = new Stack<>();
+                    System.out.println();
+                    level++;
+                }
+                
+            }
+        }
+    
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] values = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+              arr[i] = Integer.parseInt(values[i]);
+            }
+        
+            Node root = construct(arr);
+            levelOrderLinewiseZZ(root);
+        }
+}
